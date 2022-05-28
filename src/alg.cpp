f33ef276a3b82954c750f8fffea07eb14b016e72
Осинин Ярослав <99194733+Techno7721@users.noreply.github.com>
2022-05-28 19:22:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,36 @@
 // Copyright 2021 NNTU-CS
 #include  <iostream>
 #include  <fstream>
+#include  <cctype>
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+  std::string b = "";
+  char a;
+  std::ifstream file(filename);
+  while (!file.eof()) {
+    a = file.get();
+    if ((a != '.') && (a != ',') && (a != '!') &&
+(a != '?') && (a != ';') && (a != ':') &&
+(a != '"') && (a != '\'') && (a != '/') &&
+(a != '-') && (a != '[') && (a != ']') &&
+(a != '(') && (a != ')') && (a != '*') &&
+(a != '0') && (a != '1') && (a != '2') &&
+(a != '3') && (a != '4') && (a != '5') && 
+(a != '6') && (a != '7') && (a != '8') &&
+(a != '9') && (a != ' ') && (a != '\n')) {
+      a = tolower(a);
+      b += a;
+    } else {
+      if (b != "") {
+        tree.add(b);
+        b = "";
+      }
+    }
+  }
+  file.close();
+  return tree;
 }
